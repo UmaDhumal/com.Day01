efc0a25e1fb0ef10fbe3e01855b448cc778af977
admn <admn@DESKTOP-TE09GOE>
2023-01-24 14:54:03 +0530

    by Uma

---
+++ GITHUBproject/src/com/Day01/Demo.java
@@ -0,0 +1,11 @@
+package com.Day01;
+
+public class Demo 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("GIT Demo");
+		
+	}
+
+}
---
+++ GITHUBproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author admn
+ *
+ */
+module BREAKcontiue {
+}
