1b13e46514a223ebea728a3a49f9782ddec1a585
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-24 14:56:30 +0530

    by Abhya

--- GITHUBproject/src/com/Day01/Demo.java
+++ GITHUBproject/src/com/Day01/Demo.java
@@ -2,6 +2,12 @@ package com.Day01;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("Welcome to CJC");
+	
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("GIT Demo");
