e6ecb311e96c144b225de720159ff0d5b4be269f
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-25 15:38:46 +0530

    by Shobha

--- GITHUBproject/src/com/Day01/Demo.java
+++ GITHUBproject/src/com/Day01/Demo.java
@@ -12,6 +12,11 @@ public class Demo
 		System.out.println("Welcome to World");
 	
 	}
+	public void m3()
+	{
+		System.out.println("Welcome to class");
+	
+	}
 	
 	public static void main(String[] args)
 	{
